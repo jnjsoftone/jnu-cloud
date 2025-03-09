1a5398b87ad512215d6fb69fd9a77c10eeabc0ba
JnJ One <jnjsoft.one@gmail.com>
2025-03-09 20:07:45 +0900

    index.ts

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+export {
+    readJsonFromGithub,
+    uploadJsonToGithub,
+    listFilesInDirectory,
+    deleteFileFromGithub,
+} from './github.js';
+
+export {
+    createNotionClient,
+    createDatabase,
+    retrieveDatabase,
+    updateDatabase,
+    createPage,
+    retrievePage,
+    updatePage,
+    retrieveBlock,
+    updateBlock,
+    deleteBlock,
+    flatten,
+    listDatabases,
+} from './notion.js';
+
+export {
+    initSupabase,
+    getSupabase,
+    // Database
+    select,
+    selectOne,
+    insert,
+    update,
+    remove,
+    upsert,
+    // Authentication
+    signUp,
+    signIn,
+    signOut,
+    getCurrentUser,
+} from './supabase.js';
+
+// export {
+//     initAirtable,
+//     getBase,
+//     selectAll,
+//     selectOne,
+//     insert,
+//     update,
+//     remove,
+//     upsert,
+//     replace,
+// } from './airtable.js';
