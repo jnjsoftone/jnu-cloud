ee8f85eccee108045d185d788d0b26d01cd9022b
JnJ One <jnjsoft.one@gmail.com>
2025-03-10 09:59:14 +0900

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,41 @@
 export {
-    readJsonFromGithub,
-    uploadJsonToGithub,
-    listFilesInDirectory,
-    deleteFileFromGithub,
+  readJsonFromGithub,
+  uploadJsonToGithub,
+  listFilesInDirectory,
+  deleteFileFromGithub,
+  copyFolderToLocal,
 } from './github.js';
 
 export {
-    createNotionClient,
-    createDatabase,
-    retrieveDatabase,
-    updateDatabase,
-    createPage,
-    retrievePage,
-    updatePage,
-    retrieveBlock,
-    updateBlock,
-    deleteBlock,
-    flatten,
-    listDatabases,
+  createNotionClient,
+  createDatabase,
+  retrieveDatabase,
+  updateDatabase,
+  createPage,
+  retrievePage,
+  updatePage,
+  retrieveBlock,
+  updateBlock,
+  deleteBlock,
+  flatten,
+  listDatabases,
 } from './notion.js';
 
 export {
-    initSupabase,
-    getSupabase,
-    // Database
-    select,
-    selectOne,
-    insert,
-    update,
-    remove,
-    upsert,
-    // Authentication
-    signUp,
-    signIn,
-    signOut,
-    getCurrentUser,
+  initSupabase,
+  getSupabase,
+  // Database
+  select,
+  selectOne,
+  insert,
+  update,
+  remove,
+  upsert,
+  // Authentication
+  signUp,
+  signIn,
+  signOut,
+  getCurrentUser,
 } from './supabase.js';
 
 // export {
