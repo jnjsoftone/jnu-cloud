cdd4f6a9ccddfd8f72fd76fa9bc1cdffc0d791bb
JnJ One <jnjsoft.one@gmail.com>
2025-03-10 09:59:44 +0900

    index.ts에 copyFolderToLocal 추가

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-export { readJsonFromGithub, uploadJsonToGithub, listFilesInDirectory, deleteFileFromGithub, } from './github.js';
+export { readJsonFromGithub, uploadJsonToGithub, listFilesInDirectory, deleteFileFromGithub, copyFolderToLocal, } from './github.js';
 export { createNotionClient, createDatabase, retrieveDatabase, updateDatabase, createPage, retrievePage, updatePage, retrieveBlock, updateBlock, deleteBlock, flatten, listDatabases, } from './notion.js';
 export { initSupabase, getSupabase, select, selectOne, insert, update, remove, upsert, signUp, signIn, signOut, getCurrentUser, } from './supabase.js';
 //# sourceMappingURL=index.d.ts.map
